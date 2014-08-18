7f48658fc7113d1c696052ba81ba3b280255edc5
Jack <jack.kwong@renova.com.hk>
2014-08-18 11:36:44 +0800

    1st commit

---
+++ src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png",
+    DemoHead_UI_JSON : "res/DemoHead_UI/DemoHead_UI.json"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
